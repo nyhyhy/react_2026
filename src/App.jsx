5f76604e12c1627c41413cf7837107fcedb33b23
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-05 09:19:53 +0900

    #3 강의목록 HTML 및 CSS 추가

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,36 @@ function App() {
 
   return (
     <>
-      <img src='/img/htmlcss.png' />
-      <AppCounter />
+      <main>
+            <div class="card">
+                <div class="card__header">강의 목록</div>
+                <div class="card__body">
+                    <div class="courses">
+                        <article class="course">
+                            <img class="course__img" src="./img/htmlcss.png" alt="" />
+                            <div class="course__body">
+                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                            </div>
+                        </article>
+                        <article class="course">
+                            <img class="course__img" src="./img/htmlcss.png" alt="" />
+                            <div class="course__body">
+                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                            </div>
+                        </article>
+                        <article class="course">
+                            <img class="course__img" src="./img/htmlcss.png" alt="" />
+                            <div class="course__body">
+                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
+                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </main>
     </>
   )
 }
