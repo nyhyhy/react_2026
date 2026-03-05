05e0bf0ec1d74ba2573cdc6f2b7c50fa603a02a3
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-05 09:33:27 +0900

    #4 강의 목록 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import AppCounter from './AppCounter'
+import CourseListCard from './components/course/CourseListCard'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,35 +9,8 @@ function App() {
   return (
     <>
       <main>
-            <div class="card">
-                <div class="card__header">강의 목록</div>
-                <div class="card__body">
-                    <div class="courses">
-                        <article class="course">
-                            <img class="course__img" src="./img/htmlcss.png" alt="" />
-                            <div class="course__body">
-                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
-                            </div>
-                        </article>
-                        <article class="course">
-                            <img class="course__img" src="./img/htmlcss.png" alt="" />
-                            <div class="course__body">
-                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
-                            </div>
-                        </article>
-                        <article class="course">
-                            <img class="course__img" src="./img/htmlcss.png" alt="" />
-                            <div class="course__body">
-                            <div class="course__title">입문자를 위한, HTML&CSS 웹 개발 입문</div>
-                            <div class="course__description">웹 개발에 필요한 기본 지식을 배웁니다. </div>
-                            </div>
-                        </article>
-                    </div>
-                </div>
-            </div>
-        </main>
+        <CourseListCard />
+      </main>
     </>
   )
 }
