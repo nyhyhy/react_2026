35328e8b649acda530ed4a4287c00dc3a52a139b
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-05 12:25:06 +0900

    #5 컴포넌트에 props 전달하기

--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -2,7 +2,7 @@ import CourseItem from "./CourseItem";
 
 function CourseListCard() {
     return (
-        <div className="card">
+        <div className="card" style={{backgroundColor: 'grey'}}>
             <div className="card__header">강의 목록</div>
             <div className="card__body">
                 <div className="courses">
