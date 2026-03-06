70b79053c874a602ba31ec879b786a3a62306589
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-06 11:37:06 +0900

    #8 리스트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ function App() {
 
   const items = [
     {
+        id: 0,
         title: '입문자를 위한, HTML&CSS 웹 개발 입문',
         description: '웹 개발에 필요한 기본 지식을 배웁니다.',
         thumbnail: '/img/htmlcss.png',
-        ifFavorite: true
+        ifFavorite: true,
+        link: 'https://inf.run/JxyyT'
     },
     {
+        id: 1,
         title: '입문자를 위한, ES6+ 최신 자바스크립트 입문',
         description: '쉽고! 알찬! 내용을 준비했습니다.',
         thumbnail: '/img/js.png',
@@ -21,6 +24,7 @@ function App() {
         link: 'https://inf.run/Kpnd'
     },
     {
+        id: 3,
         title: '포트폴리오 사이트 만들고 배포까지!',
         description: '포트폴리오 사이트를 만들고 배포해 보세요.',
         thumbnail: '/img/portfolio.png',
@@ -29,10 +33,12 @@ function App() {
     }
   ]
 
+  const favoriteItems = items.filter(item => item.ifFavorite);
   return (
     <>
-      <main>
-        <CourseListCard items={items} />
+      <main style={{flexDirection: 'column', gap:'1rem'}}>
+        <CourseListCard title="강의 목록" items={items} />
+        <CourseListCard title="관심 강의 목록" items={favoriteItems} />
       </main>
     </>
   )
--- src/components/course/CourseListCard.jsx
+++ src/components/course/CourseListCard.jsx
@@ -1,17 +1,29 @@
+import { Fragment } from "react";
 import Card from "../Card";
 import CourseItem from "./CourseItem";
 
-function CourseListCard({ items }) {
+function CourseListCard({ title, items }) {
 
-    const [course1, course2, course3] = items;
+    //const [course1, course2, course3] = items;
+    let lastIndex = items.length - 1;
 
     return (
         <>
-            <Card title="강의 목록">
+            <Card title={title}>
                 <div className="courses">
-                    <CourseItem {...course1} />
+                    {/* <CourseItem {...course1} />
                     <CourseItem {...course2} />
-                    <CourseItem {...course3} />
+                    <CourseItem {...course3} /> */}
+                    {/* {items.map((item) => {
+                        return <CourseItem {...item} />
+                    })} */}
+                    {/* {items.map(item => <CourseItem key={item.id} {...item}/> )} */}
+                    { items.map((item, index) => (
+                        <Fragment key={item.id}>
+                            <CourseItem  {...item}/>
+                            { index !== lastIndex && <hr className="divider" />}
+                        </Fragment>
+                    ))}
                  </div>
             </Card>
         </>
