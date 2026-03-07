ba39a440fe8f9b7a059068d8966173d40198560c
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-07 22:08:59 +0900

    #14 스냅샷개념

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -16,9 +16,15 @@ export default function Counter({onTotal}) {
     // }
     const [counter, setCounter] = useState(0);
     
-    console.log('Counter>', counter);
+    console.log('[렌더링]Counter:', counter);
+
     const handleCounter = () => {
         setCounter(counter + 1);
+        setCounter(counter + 1);
+        setCounter(counter + 1);
+
+        console.log('[함수호출]Counter:', counter);
+
         if(onTotal) {
             onTotal();
         }
--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 //import App from './App.jsx'
-//import AppCounter from './AppCounter.jsx'
+import AppCounter from './AppCounter.jsx'
 //import App from './AppPure.jsx'
-import App from './AppTodo.jsx'
+//import App from './AppTodo.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <AppCounter /> */}
-    <App />
+    <AppCounter />
+    {/* <App /> */}
   </StrictMode>
 )
