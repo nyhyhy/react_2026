08a8843f51ea41e7eb20141d69e06ab4447b503a
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-09 09:10:15 +0900

    #16 State 객체업데이트

--- src/components/course/CourseForm.jsx
+++ src/components/course/CourseForm.jsx
@@ -1,19 +1,53 @@
+import { useState } from "react";
 import Card from "../Card";
 
 const CourseForm = () => {
+    const [form, setForm] = useState({
+        title: '리액트강의',
+        description: '리액트 기초부터 실전까지!'
+    })
 
     function handleCourseForm(e) {
         e.preventDefault();
+    }
+
+    // function handleTitleChange(e) {
+    //     setForm({
+    //         ...form,
+    //         title: e.target.value
+    //     });
+    // }
+
+    // const handleDescriptionChange = (e) => {
+    //     setForm({
+    //         ...form,
+    //         description: e.target.value
+    //     });
+    // }
+
+    const handleChange = (e) => {
+
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
     }
 
     return (
         <>
             <Card title="강의 등록">
                 <form style={{display: 'flex', flexDirection:'column', gap:'1rem'}} onSubmit={handleCourseForm}>
-                    <input type="text" placeholder="강의 제목" />
-                    <input type="text" placeholder="강의 한줄 설명" />
+                    <input name="title" type="text" placeholder="강의 제목" value={form.title} onChange={handleChange}/>
+                    <input name="description" type="text" placeholder="강의 한줄 설명"  value={form.description} onChange={handleChange}/>
                     <input type="submit" value="등록" />
                 </form>
+                {
+                (form.title || form.description)
+                  && (<div style={{marginTop: '16px', padding: '16px', backgroundColor: '#eee', borderRadius: '6px' }}>
+                        {form.title && (<p>제목: {form.title}</p>)}
+                        {form.description && (<p>설명: {form.description}</p>)}
+                    </div>)
+                 }
             </Card>
         </>
     );
