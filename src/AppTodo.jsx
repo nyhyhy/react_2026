45c987278b5ca235b4058f45b70f5d58d91bdf34
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-09 11:35:24 +0900

    #18 배열 업데이트 map(), filter(), slice() 활용

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -4,16 +4,86 @@ import TodoList from './components/todo/TodoList';
 
 const AppTodo = () => {
 
+    const[todoText, setTodoText] = useState('');
     const [todos, setTodos] = useState([
-        {id: 0, label: 'HTML&CSS 공부하기'},
-        {id: 1, label: '자바스크립트 공부하기'}
-    ])
+        {id: 0, text: 'HTML&CSS 공부하기', done: false},
+        {id: 1, text: '자바스크립트 공부하기', done: false}
+    ]);
+    const [insertAt, setInsertAt] = useState(todos.length -1);
+
+    const handleAddTodo = () => {
+        const nextId = todos.length;
+        // todos.push({id: nextId, text: ''});
+        setTodos([
+            ...todos,
+            {id: nextId, text: todoText, done: false}
+        ]);
+        setTodoText(''); // null, undefined 동작안함
+    }
+
+    const handleAddTodoByIndex = () => {
+        const nextId = todos.length;
+        const newTodos = [
+            // 삽입 지점 이전 항목
+            ...todos.slice(0, insertAt),
+            {id: nextId, text: todoText, done: false},
+            // 삽입 지점 이후 항목
+            ...todos.slice(insertAt)
+        ];
+        setTodos(newTodos);
+        setTodoText('');
+    }
+
+    const handleTodoTextChange = (e) => {
+        setTodoText(e.target.value);
+    }
+
+    const handleDeleteTodo = (deleteId) => {
+        const newTodos = todos.filter(item => item.id != deleteId);
+        setTodos(newTodos);
+    }
+
+    const handleTodoKeyDown = (e) => {
+        if(e.key === 'Enter' && todoText !== '') {
+            handleAddTodo();
+        }
+    }
+
+    const handleToggleTodo = (id, done) => {
+        // 기존 배열 안의 객체 속성을 변경
+        const nextTodos = todos.map(item => {
+            if(item.id === id) {
+                return {
+                    ...item,
+                    done: done
+                };
+            }
+            return item;
+        });
+        setTodos(nextTodos);
+    }
 
     return (
         <div>
             <h2>할일목록</h2>
-            <TodoList  todos={todos}/>
-            <TodoList  todos={todos}/>
+            <div>
+                <input type="text" value={todoText} onChange={handleTodoTextChange} onKeyDown={handleTodoKeyDown}/>
+                <button onClick={handleAddTodoByIndex}>추가</button>
+            </div>
+            <div>
+                <select value={insertAt} onChange={(e) => (setInsertAt(e.target.value))}>
+                    {todos.map((item, index) => (
+                        <option key={item.id} value={index}>{index} 번째</option>
+                    ))}
+                </select>
+                <button onClick={handleAddTodoByIndex}>{insertAt} 번째 추가</button>
+            </div>
+            <div>Preview: {todoText}</div>
+            <TodoList 
+                todos={todos} 
+                onDeleteTodo={handleDeleteTodo} 
+                onToggleTodo={handleToggleTodo} 
+            />
         </div>
     );
 }
--- src/components/todo/TodoList.jsx
+++ src/components/todo/TodoList.jsx
@@ -1,12 +1,25 @@
-function TodoList({ todos = [] }) {
-    const items = [...todos];
-    items.push({id: 2, label: '포트폴리오 사이트 만들기'});
+function TodoList({ todos = [], onDeleteTodo, onToggleTodo }) {
+    // const items = [...todos];
+    // items.push({id: 2, text: '포트폴리오 사이트 만들기'});
 
     return (
         <ul>
-            {items.map((item) => (
-                <li key={item.id}>{item.label}</li>
-            ))}
+            {/* {todos.map((item) => (
+                <li key={item.id}>{item.text}</li>
+            ))} */}
+            {todos.map((item) => {
+                return (
+                    <>
+                        <li key={item.id}>
+                            <input type="checkbox"
+                                 checked={item.done} 
+                                 onChange={(e) => onToggleTodo(item.id, e.target.checked)}/>
+                            <span>{item.done ? (<del>{item.text}</del>) : item.text}</span>
+                            <button onClick={() => onDeleteTodo(item.id)}>X</button>
+                        </li>
+                    </>
+                )
+            })}
         </ul>
     );
 
