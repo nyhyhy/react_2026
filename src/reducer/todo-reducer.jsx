144ea9045288de80821195a86c0867f818adc237
JiYeong <51521293+nyhyhy@users.noreply.github.com>
2026-03-12 10:11:44 +0900

    #22 Reducer에 Immer의 간결함을 더하기

--- src/reducer/todo-reducer.jsx
+++ src/reducer/todo-reducer.jsx
@@ -1,43 +1,28 @@
-export default function todoReducer(todos, action) {
+export default function todoReducer(draft, action) {
     // type
     switch(action.type) {
         case 'added': {
             const { nextId, todoText } = action;
-            return [
-                ...todos,
-                {id: nextId, text: todoText, done: false}
-            ];
+            draft.push({id: nextId, text: todoText, done: false})
+            break;
         }
         case 'added_index' : {
             const { insertAt, nextId, todoText } = action;
-            return [
-                // 삽입 지점 이전 항목
-                ...todos.slice(0, insertAt),
-                // 새 항목
-                {id: nextId, text: todoText, done: false},
-                // 삽입 지점 이후 항목
-                ...todos.slice(insertAt)
-            ];
+            draft.splice(insertAt, 0, {id: nextId, text: todoText, done: false})
+            break;
         }
         case 'deleted' : {
             const { deleteId } = action;
-            return todos.filter(item => item.id != deleteId);
-
+            return draft.filter(item => item.id != deleteId);
         }
         case 'done' : {
             const { id, done } = action;
-            return todos.map(item => {
-                if(item.id === id) {
-                    return {
-                        ...item,
-                        done: done
-                    };
-                }
-                return item;
-            });
+            draft.find(item => item.id === id);
+            target.done = done;
+            break;
         }
         case 'reverse' :{
-            return todos.toReversed();
+            return draft.toReversed();
         }
         default: {
             throw Error('알 수 없는 액션 type:' + action.type)
